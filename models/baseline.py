6eecd541c7565805e7b02e558171a8e2de682593
PriyaDCosta <76833529+PriyaDCosta@users.noreply.github.com>
2024-02-24 16:00:52 -0500

    tried importing submodule

--- models/baseline.py
+++ models/baseline.py
@@ -1,1 +1,16 @@
-import pandas as pd
+import os
+import sys
+from importlib import import_module
+
+# # Get the current working directory
+# current_directory = os.getcwd()
+
+# # Get the parent directory
+# parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+
+# # Add the parent directory to the Python path
+# sys.path.append(parent_directory)
+
+# Import the module dynamically
+fflow = import_module('team-process-map.feature_engine.features.fflow').get_forward_flow
+print(type(fflow))
